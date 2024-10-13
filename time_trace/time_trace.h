9dcaec0914feaea53139b5a376313caad43d5814
Guido Fuchs <fuchs.guido@web.de>
2024-10-13 20:36:49 +0200

    changed time stamps to array, including rudimentary missuse safety.

--- time_trace/time_trace.h
+++ time_trace/time_trace.h
@@ -1,23 +1,44 @@
 #pragma once
 #include <chrono>
 #include <cstdint>
+#include <array>
 
 namespace time_trace {
 
 
  //   const std::chrono::duration<double> elapsed_seconds{end - start};
  
+  #define unlikely_branch(x)    __builtin_expect (!!(x), 0)
+
   using time_point = std::chrono::time_point<std::chrono::steady_clock>;
+  constexpr uint32_t nr_time_stamps{4u};
+  constexpr uint32_t i_start{0u};
 
   class time_trace {
-    time_point t_start;
-    time_point t_stop;
-  public:
-    inline void start() { t_start = std::chrono::steady_clock::now(); }
-    inline void stop() { t_stop = std::chrono::steady_clock::now(); }
-    inline void next();
-    inline void loop();
-    inline int64_t delta() { return std::chrono::duration_cast<std::chrono::nanoseconds>(t_stop - t_start).count();}
+    
+    time_point time_stamps[nr_time_stamps];
+    const uint32_t i_stop = nr_time_stamps - 1u;
+    uint32_t i{i_start};
 
+  public:
+    inline void start() { 
+      if (unlikely_branch( i != i_start )) std::exit(EXIT_FAILURE);
+      measure(); 
+    }
+    inline void stop() { 
+        i = i_stop;
+        measure(); 
+        i = 0;
+      }
+    inline void next() { 
+        measure();
+        if (unlikely_branch( i >= nr_time_stamps )) std::exit(EXIT_FAILURE);
+      }
+    inline void loop();
+    inline int64_t delta() { return std::chrono::duration_cast<std::chrono::nanoseconds>(time_stamps[i_stop] - time_stamps[i_start]).count();}
+    inline void measure() { 
+        time_stamps[i] = std::chrono::steady_clock::now();
+        i++;
+      }
   };
 } // namespace time_trace
